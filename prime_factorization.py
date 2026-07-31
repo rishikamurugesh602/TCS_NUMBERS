5d1b9ea89b64f9856493fbe64bee7f71c751f34e
rishikamurugesh602 <rishikamurugesh@gmail.com>
2026-07-31 17:00:23 +0530

    Create prime_factorization.py

---
+++ prime_factorization.py
@@ -0,0 +1,15 @@
+n=int(input())
+def prime_factors(n):
+    res=[]
+    i = 2
+
+    while i * i <= n:
+        while n % i == 0:
+            res.append(i)
+            n //= i
+        i += 1
+
+    if n > 1:
+        res.append(n)
+    return res
+print(prime_factors(n))
